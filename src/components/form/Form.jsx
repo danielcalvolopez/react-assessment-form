82ff0923aed59773ca5ebb17ab827a3c12e9d905
Daniel <acidistrict@gmail.com>
2023-05-31 20:13:44 +0100

    remove unused props

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,7 @@
 import Button from "../UI/Button";
 import classes from "./form.module.css";
 
-const Form = ({ children, onSubmit, error }) => {
+const Form = ({ children, onSubmit }) => {
   return (
     <form className={classes.form} onSubmit={onSubmit}>
       {children}
